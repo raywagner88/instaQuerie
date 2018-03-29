999f25031179fdf68ac67deba8dabd9bb635e568
Ray Wagner <raywagner88@me.com>
2018-03-29 15:04:03 -0500

    Added image filters

--- script.js
+++ script.js
@@ -6,23 +6,40 @@ $(document).ready(function() {
   const IMG_WIDTH = $('#img-width');
   const IMG_HEIGHT = $('#img-height');
   const IMG_FILTER = $('#img-filter');
-  const FILTER_OPTIONS = $('.filter-options')
+  const FILTER_OPTIONS = $('.filter-options');
+  const SEPIA = $('#sepia');
+  const SEPIA_FILTER = $('#img-sepia');
+  const BRIGHTNESS = $('#brightness');
+  const BRIGHTNESS_FILTER = $('#img-brightness');
+  const GRAYSCALE = $('#grayscale');
+  const GRAYSCALE_FILTER = $('#img-grayscale');
+  const CONTRAST = $('#contrast');
+  const CONTRAST_FILTER = $('#img-contrast');
+  const INVERT = $('#invert');
+  const INVERT_FILTER = $('#img-invert');
+  const SATURATE = $('#saturate');
+  const SATURATE_FILTER = $('#img-saturate');
+
 
   LOAD_PICTURE.click(function() {
     PICTURE.attr('src', IMG_URL.val())
       .attr('width', IMG_WIDTH.val())
       .attr('height', IMG_HEIGHT.val());
+    SEPIA_FILTER.css('filter', `sepia(${SEPIA.val()}%)`);
+    BRIGHTNESS_FILTER.css('filter', `brightness(${BRIGHTNESS.val()}%)`);
+    GRAYSCALE_FILTER.css('filter', `grayscale(${GRAYSCALE.val()}%)`);
+    CONTRAST_FILTER.css('filter', `contrast(${CONTRAST.val()}%)`);
+    INVERT_FILTER.css('filter', `invert(${INVERT.val()}%)`);
+    SATURATE_FILTER.css('filter', `saturate(${SATURATE.val()}%)`);
   });
 
   IMG_FILTER.change(function(e) {
     FILTER_OPTIONS.each(function() {
-      console.log($(this));
-      if ($(this) == e.target.value) {
+      if ($(this).attr('id') == e.target.value) {
         $(this).removeClass('hidden');
       } else {
         $(this).addClass('hidden');
       }
     });
-    console.log(e.target);
   });
 });
